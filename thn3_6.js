e35431f7d23b4218ced50e09135dca0474ca650e
luyenthi <nongaphao@gmail.com>
2026-04-19 21:48:29 +0900

    Add PRACTICE_SETS for new question set

--- thn3_6.js
+++ thn3_6.js
@@ -1,5 +1,6 @@
-window.QUESTION_SETS = window.QUESTION_SETS || {};
-window.QUESTION_SETS["n3_6"] = {
+window.PRACTICE_SETS = window.PRACTICE_SETS || {};
+
+window.PRACTICE_SETS["g3_6"] = {
   shuffle: true,
   questions: [
   {
